db84c887218f1de3483e4982e8771d37bd1cde4a
codegino <carloginocatapang@gmail.com>
2021-11-23 09:30:36 +0800

    Created pokemons route

---
+++ app/routes/pokemons/index.tsx
@@ -0,0 +1,29 @@
+import { Link, LoaderFunction, useLoaderData } from "remix";
+
+export const loader: LoaderFunction = () => {
+  return fetch("https://pokeapi.co/api/v2/pokemon");
+};
+
+export function meta() {
+  return {
+    title: "Pokemons",
+    description: "List of Pokemons",
+  };
+}
+
+export default function Pokemons() {
+  const data = useLoaderData();
+
+  return (
+    <div>
+      <h2>Pokemons</h2>
+      <ul>
+        {data.results.map((pokemon) => (
+          <li key={pokemon.name}>
+            <Link to={`/pokemons/${pokemon.name}`}>{pokemon.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
